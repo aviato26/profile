4a01562b4589a106a893d230db93fe332850d77e
shaun deshler <shaun@shauns-iMac.local>
2019-04-08 22:56:41 -0700

    working on vertical card flip

--- website/jscript.js
+++ website/jscript.js
@@ -1,12 +1,23 @@
 
-document.addEventListener('touchstart', (e) => {
-  console.log(e.type)
-})
+let articles = {
+  nodes: [...document.querySelectorAll('article')],
+  clientY: 0,
+  switchCard(int){
+    this.nodes[int].className = 'card'
+  }
+}
 
-document.addEventListener('touchmove', (e) => {
-  console.log(e.type)
-})
+articles.nodes.map((c, i) => {
+  c.addEventListener('touchstart', (e) => {
+    articles.clientY = e.changedTouches[0].clientY - e.changedTouches[0].screenY - 400
+  })
 
-document.addEventListener('touchend', (e) => {
-  console.log(e.type)
+  c.addEventListener('touchmove', (e) => {
+  articles.clientY = e.changedTouches[0].clientY - e.changedTouches[0].screenY - 400
+  articles.nodes[i].style.transform = `translateY(${articles.clientY}px)`
+  })
+
+  c.addEventListener('touchend', (e) => {
+  articles.switchCard(i)
+  })
 })
