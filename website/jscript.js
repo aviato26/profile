60b2bb376ae9b549830270085acd873a816e925c
shaun deshler <shaun@shauns-iMac.local>
2019-04-08 17:37:57 -0700

    initial layout

--- website/jscript.js
+++ website/jscript.js
@@ -1,4 +1,12 @@
 
+document.addEventListener('touchstart', (e) => {
+  console.log(e.type)
+})
+
 document.addEventListener('touchmove', (e) => {
-  console.log(e)
+  console.log(e.type)
+})
+
+document.addEventListener('touchend', (e) => {
+  console.log(e.type)
 })
