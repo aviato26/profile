3197033dfca17f501cd0dd3fac7de2cf0aba9ca8
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2019-04-11 21:18:30 -0700

    changed second half of animation to css keyframes

--- website/jscript.js
+++ website/jscript.js
@@ -7,10 +7,12 @@ let articles = {
     if(this.clientY < -30){
       this.nodes.filter((c, i) => {
         if(int === i){
-          c.className = 'card'
+          c.className = 'card flipback'
 
           // setting y position to 0 or else position remains the same
-          c.style.transform = 'translateY(0px)'
+          //c.style.transform = 'translateY(0px)'
+          c.style.setProperty('--posY', this.clientY)
+          c.style.setProperty('--rotX', this.rotate)
         } else {
           c.className = 'card currentCard';
         }
@@ -24,12 +26,6 @@ let articles = {
     */
     return (this.clientY < 10) ? this.rotate+=50 : this.rotate = 0
   },
-  card(){
-    if(this.clientY < 0){
-      this.clientY+= 0.01
-      requestAnimationFrame(this.card())
-    }
-  }
 }
 
 articles.nodes.map((c, i) => {
@@ -51,6 +47,5 @@ articles.nodes.map((c, i) => {
   c.addEventListener('touchend', (e) => {
     // as the event ends we will switch the z-index of the current and next article nodes
     articles.switchCard(i)
-    articles.card()
   })
 })
