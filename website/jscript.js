8117153484fd9e9e2c2caa0e36614743bb2da6d6
shaun deshler <shaun@shauns-iMac.local>
2019-04-12 21:59:13 -0700

    card flip animation working, starting on page transitions

--- website/jscript.js
+++ website/jscript.js
@@ -2,6 +2,7 @@
 let articles = {
   nodes: [...document.querySelectorAll('article')],
   clientY: 0,
+  clientX: 0,
   rotate: 0,
   switchCard(int){
     if(this.clientY < -30){
@@ -25,17 +26,20 @@ let articles = {
       so i increased the rotate velocity to 50 instead of 3
     */
     return (this.clientY < 10) ? this.rotate+=50 : this.rotate = 0
-  },
+  }
 }
+
+let section = document.querySelector('.dude');
 
 articles.nodes.map((c, i) => {
   c.addEventListener('touchstart', (e) => {
-    articles.clientY = e.changedTouches[0].clientY - e.changedTouches[0].screenY - 400
+    articles.clientY = e.changedTouches[0].clientY - e.changedTouches[0].screenY - 400;
   })
 
   c.addEventListener('touchmove', (e) => {
-    articles.clientY = e.changedTouches[0].clientY - e.changedTouches[0].screenY - 400
-
+    articles.clientY = e.changedTouches[0].clientY - e.changedTouches[0].screenY - 400;
+    articles.clientX = e.changedTouches[0].clientX - window.innerWidth / 2;
+    
     /*
       as the client moves up the y axis we will update the activated article node
       with the events current position
@@ -45,7 +49,14 @@ articles.nodes.map((c, i) => {
   })
 
   c.addEventListener('touchend', (e) => {
+
     // as the event ends we will switch the z-index of the current and next article nodes
     articles.switchCard(i)
+  })
+
+  c.addEventListener('animationend', (e) => {
+
+    //after animation ends resetting position
+    c.style.transform = 'translateY(0px) rotateX(0deg)'
   })
 })
