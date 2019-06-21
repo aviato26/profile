568b61660a3955a601d69b0f81654d5d1c9f12bb
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2019-06-20 22:08:30 -0700

    adding anime js

--- website/jscript.js
+++ website/jscript.js
@@ -114,3 +114,15 @@ mobileContainer.forEach((c, i) => {
     }
   })
 })
+
+
+let header = document.querySelector('header');
+
+anime({
+  targets: 'header',
+  scale: [
+    {value: .1, easing: 'easeOutSine', duration: 500},
+    {value: 1, easing: 'easeInOutQuad', duration: 1200}
+  ],
+  delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
+});
