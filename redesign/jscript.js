91aa0e99c55a822c762b7257c352c57d741d0a0f
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2019-06-26 23:52:22 -0700

    added pages from old design

--- redesign/jscript.js
+++ redesign/jscript.js
@@ -1,20 +1,22 @@
 
 let list = document.querySelectorAll('li');
 let infoPage = document.querySelector('.hide');
+let container = document.querySelector('.container');
 
 let navAnimation = () => {
   anime({
     targets: list,
-    translateX: -570,
+    translateX: -(container.offsetWidth / 2) / 2,
     delay: anime.stagger(200)
   })
 }
 
 list.forEach(c => {
   c.addEventListener('click', (e) => {
+    console.log((container.offsetWidth / 2) / 2)
     navAnimation()
     setTimeout(() => {
-      infoPage.className = 'stage'
+      infoPage.className = 'stage bounce-7'
     }, 2000)
   })
 })
