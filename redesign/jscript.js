dd3d8075b083fa104746f1f6c7124305f3484d09
shaun deshler <shaun@shauns-iMac.local>
2019-06-27 21:49:20 -0700

    set bouncing animation for everytime user clicks li elements, still needs typewriter effect to stop once text is done

--- redesign/jscript.js
+++ redesign/jscript.js
@@ -2,6 +2,7 @@
 let list = document.querySelectorAll('li');
 let infoPage = document.querySelector('.hide');
 let container = document.querySelector('.container');
+let articles = document.querySelectorAll('article')
 
 let navAnimation = () => {
   anime({
@@ -11,12 +12,33 @@ let navAnimation = () => {
   })
 }
 
-list.forEach(c => {
+list.forEach((c, i) => {
   c.addEventListener('click', (e) => {
-    console.log((container.offsetWidth / 2) / 2)
+    // we will manipulate this int for the bouncing animation when the li links are clicked
+
+    let timer = 1500;
+
+    // sliding animation for the nav bar
     navAnimation()
+
+    /*
+      we will set all articles to display none and then set clicked element to display block
+      while also resetting the timer int so the setTimeout function will run every time the
+      li links are clicked
+    */
+    articles.forEach((c, i) => {
+      c.style.display = 'none';
+    })
+    articles[i].style.display = 'block'
+    articles[i].className = 'box'
+    if(infoPage.className === 'stage bounce-7'){
+      infoPage.className = 'stage'
+      timer = 100
+    }
+
+  // after a second and a half this will display the main section element
     setTimeout(() => {
       infoPage.className = 'stage bounce-7'
-    }, 2000)
+    }, timer)
   })
 })
